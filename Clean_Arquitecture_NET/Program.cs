386d99e2d020e36bc1315c2378ba6f9231283f76
gianpvp99 <gianpvp99@gmail.com>
2024-09-22 11:27:44 -0500

    Agregar archivos de proyecto.

---
+++ Clean_Arquitecture_NET/Program.cs
@@ -0,0 +1,35 @@
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.Development.json").AddEnvironmentVariables().Build();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var environment = builder.Environment;
+if (environment.IsProduction())
+{
+
+}
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
